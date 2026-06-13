994d7e00919c3dcc913fc43f819b2732fc707e46
dianka <109206311+dkim08@users.noreply.github.com>
2026-06-13 17:14:05 +0500

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,20 +8,39 @@ const Footer: React.FC = () => {
                 <h1>BeautySpace твоё пространство ухода и красоты каждый день</h1>
                 <div className="footer-content">
                     <div className="left-footer">
-                        <p> <span>Соцсети</span> 
-                            <b>Instagram:</b> <a href="https://www.instagram.com/">@beautyspace_kz</a> 
-                            <b>Telegram:</b> <a href="https://web.telegram.org/"> @beautyspace</a>
-                        </p>
+                        <span>Соцсети</span>
+
+                        <div className="footer-item">
+                            <b>Instagram:</b>
+                            <a href="https://www.instagram.com/" target="_blank">
+                                @beautyspace_kz
+                            </a>
+                        </div>
+
+                        <div className="footer-item">
+                            <b>Telegram:</b>
+                            <a href="https://web.telegram.org/" target="_blank">
+                                @beautyspace
+                            </a>
+                        </div>
                     </div>
+
                     <div className="right-footer">
-                        <p> <span>Контакты</span>
-                            <b>Телефон</b> <a href="https://wa.me/77077106633" target="_blank">+7 922 003 310</a>
-                            <b>Email:</b> <a
-                                href="mailto:example@gmail.com?"
-                                target="_blank">
+                        <span>Контакты</span>
+
+                        <div className="footer-item">
+                            <b>Телефон:</b>
+                            <a href="https://wa.me/77077106633" target="_blank">
+                                +7 922 003 310
+                            </a>
+                        </div>
+
+                        <div className="footer-item">
+                            <b>Email:</b>
+                            <a href="mailto:beautyspace@mail.com">
                                 beautyspace@mail.com
                             </a>
-                        </p>
+                        </div>
                     </div>
                 </div>
                 <p className="f-p">© 2025 BeautySpace. Все права защищены.</p>
